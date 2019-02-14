a3bd1d2ca0234bc5f129d4094a8109a2ec258617
Nikita <ptznikko@gmail.com>
2019-02-14 04:40:57 +0300

    .gitignore fix readd

--- dist/js/questionsDropdown.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.questions-box__header').on('click', (e) => {
-        if(!$(e.target.parentNode).hasClass('questions-box__card_active')) {
-            $(e.target.parentNode).addClass('questions-box__card_active');
-        } else {
-            $(e.target.parentNode).removeClass('questions-box__card_active');
-        }
-    });
-});
